fbe3c61f3e77ec5b03d9530874845ca889219bd7
Reba Pipes <rebablackbox@gmail.com>
2019-07-31 18:16:53 +0000

    video embedding

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,3 @@ CarrierWave.configure do |config|
   }
 end
 
-if Rails.env.development?
-  config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-  config.root = '/home/vagrant/uploads_tmp/tmp'
-end
