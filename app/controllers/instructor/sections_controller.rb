e2fc8babc07f607012cff79fe742b6e47cc2c78a
Reba Pipes <rebablackbox@gmail.com>
2019-08-05 21:43:55 +0000

    update

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -11,7 +11,16 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
+  end
+
   private
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -20,11 +29,15 @@ class Instructor::SectionsController < ApplicationController
   end
 
   helper_method :current_course
-  def current_course
+def current_course
+  if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
   end
+end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :subtitle, :image, :row_order_position)
   end
 end
